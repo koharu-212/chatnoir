056bae41795dc6c2a924158550b65cf1196228e3
kizuna0411 <g2342114@tcu.ac.jp>
2025-02-08 17:16:45 +0900

    Delete 0130-demo directory

--- 0130-demo/backend/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-DB_USER = os.getenv("POSTGRES_USER", "your_db_user")
-DB_PASSWORD = os.getenv("POSTGRES_PASSWORD", "your_db_password")
-DB_HOST = os.getenv("POSTGRES_HOST", "db")
-DB_PORT = os.getenv("POSTGRES_PORT", "5432")
-DB_NAME = os.getenv("POSTGRES_DB", "your_db_name")
-
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
